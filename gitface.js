be6be8418e79d556a4468163a9d8abdc5f206221
Rob Stenson <rob@johnmcneilstudio.com>
2011-04-22 16:24:06 -0700

    idle tidying

--- gitface.js
+++ gitface.js
@@ -12,17 +12,15 @@
         href: "https://github.com/"+contributor.login,
         target: "_blank"
       });
+      // the image
       div.append($("<img/>",{
         src: "http://www.gravatar.com/avatar/"+contributor.gravatar_id+"?s=300"
       }));
-      div.append($("<span/>",{
-        className: "contributions",
-        text: contributor.contributions
-      }));
-      div.append($("<span/>",{
-        className: "login",
-        text: contributor.login
-      }))
+      // the number of contributions
+      div.append($("<span/>",{ className: "contributions", text: contributor.contributions }));
+      // the username
+      div.append($("<span/>",{ className: "login", text: contributor.login }));
+      // throw it in
       gitfaces.append(div);
     });
   }
@@ -46,9 +44,7 @@
       if(input.val() == _input.defaultValue) {
         input.val("");
       }
-    });
-    
-    input.blur(function(){
+    }).blur(function(){
       if(input.val().match(/^$|^[\s]+$/)) {
         input.removeClass("typing");
         input.attr("value",_input.defaultValue);
