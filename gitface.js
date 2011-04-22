42c39ef4c3ef335fbdc719871c0fc3473b1d3ad8
Rob Stenson <rob@johnmcneilstudio.com>
2011-04-22 15:57:59 -0700

    stylin them basics

--- gitface.js
+++ gitface.js
@@ -1,13 +1,68 @@
-$(document).ready(function() {
-  var match = window.location.hash.match(/#\/(\w+)\/(\w+)/);
-  user = match[1];
-  repo = match[2];
+(function(){
   
-  console.log(user, repo);
+  var gitfaces,
+    baseUrl = "http://github.com/api/v2/json/repos/show/";
   
-  $.getJSON("http://github.com/api/v2/json/repos/show/"+user+"/"+repo+"/contributors?callback=?", function(response) {
-    $.each(response.contributors, function(index, contributor) {
-      $("body").append("<img src='http://www.gravatar.com/avatar/"+contributor.gravatar_id+"?s=300' />");
+  function loadEarl() {
+    var match = window.location.hash.match(/#\/(\w+)\/(\w+)/),
+      user = match[1],
+      repo = match[2];
+    
+    $.getJSON(baseUrl+user+"/"+repo+"/contributors?callback=?", function(response) {
+      gitfaces.empty();
+      $.each(response.contributors, function(index, contributor) {
+        var div = $("<a/>",{
+          className: "gitface",
+          href: "https://github.com/"+contributor.login,
+          target: "_blank"
+        });
+        div.append($("<img/>",{
+          src: "http://www.gravatar.com/avatar/"+contributor.gravatar_id+"?s=300"
+        }));
+        div.append($("<span/>",{
+          className: "contributions",
+          text: contributor.contributions
+        }));
+        div.append($("<span/>",{
+          className: "login",
+          text: contributor.login
+        }))
+        gitfaces.append(div);
+      });
+    });
+  }
+  
+  $(document).ready(function(){
+    gitfaces = $("#gitfaces");
+    
+    var input = $("#addressbar input"),
+      _input = input[0];
+    
+    input.focus(function(){
+      input.addClass("typing");
+      if(input.val() == _input.defaultValue) {
+        input.val("");
+      }
+    });
+    
+    input.blur(function(){
+      if(input.val().match(/^$|^[\s]+$/)) {
+        input.removeClass("typing");
+        input.attr("value",_input.defaultValue);
+      }
+    });
+    
+    $("form").submit(function(event){
+      event.preventDefault();
+      (input.val().match(/(\w+)\/(\w+)/)) ?
+        window.location.href = "#/"+input.val() :
+        alert("That doesn't look like a github repo url");
+    });
+    
+    // address handling
+    $.address.change(function(){
+      loadEarl();
     });
   });
-});
+  
+})();
